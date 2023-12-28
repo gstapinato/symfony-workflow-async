3b4f773c462dac1c40121871878dcdd2c8037771
Gabriel Stapinato <gstapinato@santafe.gov.ar>
2023-12-28 10:15:31 -0300

    Add previous exception used for the exception chaining

--- src/Exception/ServiceHttpException.php
+++ src/Exception/ServiceHttpException.php
@@ -9,18 +9,18 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class ServiceHttpException extends HttpException
 {
-    protected function __construct(int $statusCode, string $message = '')
+    protected function __construct(int $statusCode, string $message = '', \Throwable|null $previous = null)
     {
-        parent::__construct($statusCode, $message);
+        parent::__construct($statusCode, $message, $previous);
     }
 
-    public static function createValidationException(string $message = ''): self
+    public static function createValidationException(string $message = '', \Throwable|null $previous = null): self
     {
-        return new self(Response::HTTP_UNPROCESSABLE_ENTITY, $message);
+        return new self(Response::HTTP_UNPROCESSABLE_ENTITY, $message, $previous);
     }
-    public static function createNotFoundException(string $message = ''): self
+    public static function createNotFoundException(string $message = '', \Throwable|null $previous = null): self
     {
-        return new self(Response::HTTP_NOT_FOUND, $message);
+        return new self(Response::HTTP_NOT_FOUND, $message, $previous);
     }
 
 }
