db01f5293e442f9964ae15c22a1f46e71ff9f314
Gabriel Stapinato <gstapinato@santafe.gov.ar>
2023-12-28 11:00:04 -0300

    fix: change use statement

--- src/DTO/ProductDTO.php
+++ src/DTO/ProductDTO.php
@@ -3,7 +3,7 @@
 namespace App\DTO;
 
 use OpenApi\Attributes as OA;
-use PhpParser\Node\Expr\BinaryOp\GreaterOrEqual;
+use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
 use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\GreaterThan;
@@ -20,9 +20,9 @@ class ProductDTO
         public ?string $name = null,
         #[NotBlank(message: "Product category tree should not be blank")]
         public ?string $productCategoryTree = null,
-        #[GreaterOrEqual(value:0, message: "Retail price should be greater than zero")]
+        #[GreaterThanOrEqual(value:0, message: "Retail price should be greater than zero")]
         public ?float $retailPrice = null,
-        #[GreaterOrEqual(value:0, message: "Discounted price should be greater than zero")]
+        #[GreaterThanOrEqual(value:0, message: "Discounted price should be greater than zero")]
         public ?float $discountedPrice = null,
         public ?string $image = null
     ) {
